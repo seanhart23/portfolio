a9859235e6053b0f899324dded279eff18a19d5c
Sean Hart <31716085+seanhart23@users.noreply.github.com>
2021-09-12 13:17:47 -0400

    content changes

--- public/js/transitions.js
+++ public/js/transitions.js
@@ -1,15 +1,11 @@
 /*** Scroll Transitons ***/
 if ($(window).width() < 768) {
-    $(window).scroll(function () {
-        console.log('Scrolling')
-        $('.navbar').toggleClass('scroll-transition', $(this).scrollTop() > 0);
-        $('.navbar-brand').toggleClass('scroll-transition-brand', $(this).scrollTop() > 0);
-        $('.active-link').toggleClass('scroll-transition-border', $(this).scrollTop() > 0);
-    });
+    $('.navbar').toggleClass('scroll-transition', $(this).scrollTop() >= -1);
+    $('.navbar-brand').toggleClass('scroll-transition-brand', $(this).scrollTop() >= -1);
+    $('.active-link').toggleClass('scroll-transition-border', $(this).scrollTop() >= -1);
 }
 else {
     $(window).scroll(function () {
-        console.log('Scrolling')
         $('.navbar').toggleClass('scroll-transition', $(this).scrollTop() > 200);
         $('.navbar-brand').toggleClass('scroll-transition-brand', $(this).scrollTop() > 200);
         $('.active-link').toggleClass('scroll-transition-border', $(this).scrollTop() > 200);
